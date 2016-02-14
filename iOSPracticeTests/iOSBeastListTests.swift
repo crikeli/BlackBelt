7fa3579a8009c6e8cbe7484859d8c4aa74be1ac0
crikeli <christik.dev@gmail.com>
2016-02-14 01:56:15 -0800

    Update iOSBeastListTests.swift

--- iOSPracticeTests/iOSBeastListTests.swift
+++ iOSPracticeTests/iOSBeastListTests.swift
@@ -2,8 +2,8 @@
 //  iOSPracticeTests.swift
 //  iOSPracticeTests
 //
-//  Created by Wei Chung Chuo on 8/13/15.
-//  Copyright © 2015 Wei Chung Chuo. All rights reserved.
+//  Created by Kelin Christi on 1/22/16.
+//  Copyright © 2016 Kelz. All rights reserved.
 //
 
 import XCTest
